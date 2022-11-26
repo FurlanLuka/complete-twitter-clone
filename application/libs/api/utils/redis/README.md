53f619d9331997d7c51fa4c9ba946cd76e22898e
Luka <luka@archie.finance>
2022-11-26 21:24:32 +0100

    Added Redis library

---
+++ application/libs/api/utils/redis/README.md
@@ -0,0 +1,3 @@
+# api-utils-redis
+
+This library was generated with [Nx](https://nx.dev).
---
+++ application/libs/api/utils/redis/src/lib/redis.interfaces.ts
@@ -0,0 +1,34 @@
+import {
+  Type,
+  DynamicModule,
+  ForwardReference,
+  InjectionToken,
+  OptionalFactoryDependency,
+} from '@nestjs/common';
+
+export enum ConnectionType {
+  SINGULAR,
+  MULTI,
+}
+
+export interface SingularConnectionOptions {
+  type: ConnectionType.SINGULAR;
+  connectionName: string;
+  connectionUrl: string;
+  keyPrefix: string;
+}
+
+export interface MultiConnectionOptions {
+  type: ConnectionType.MULTI;
+  connections: SingularConnectionOptions[];
+}
+
+export type RedisConfig = SingularConnectionOptions | MultiConnectionOptions;
+
+export interface RedisOptions {
+  imports: Array<
+    Type | DynamicModule | Promise<DynamicModule> | ForwardReference
+  >;
+  inject: (InjectionToken | OptionalFactoryDependency)[];
+  useFactory: (...args: unknown[]) => RedisConfig;
+}
